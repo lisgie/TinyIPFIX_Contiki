ea918cf450d820bdc058af3f5de71182979f9f02
lisgie <livio.sgier@uzh.ch>
2016-08-12 11:04:33 +0200

    Added debugging code with the hel of LEDs

--- node.c
+++ node.c
@@ -5,10 +5,9 @@
 
 #include "TinyIPFIX/tinyipfix.h"
 
-struct template_rec *sky_rec;
-
 PROCESS(main_proc, "Main Process");
 AUTOSTART_PROCESSES(&main_proc);
+
 
 PROCESS_THREAD(main_proc, ev, data)
 {
@@ -20,35 +19,44 @@ PROCESS_THREAD(main_proc, ev, data)
   PROCESS_BEGIN();
 
   if(conn_set_up() == -1) {
-	  leds_on(LEDS_RED);
+
+	  debug(LEDS_GREEN | LEDS_RED | LEDS_BLUE);
 	  PROCESS_EXIT();
   }
 
   initialize();
 
   // Set event timers for template and data packet creation
-  etimer_set(&template_timer, CLOCK_SECOND*15);
-  etimer_set(&data_timer, CLOCK_SECOND*5);
+  etimer_set(&template_timer, CLOCK_SECOND*16);
+  etimer_set(&data_timer, CLOCK_SECOND*7);
 
   while (1) {
 
 	  PROCESS_WAIT_EVENT();
       if (etimer_expired (&template_timer)) {
 
-    	  //buffer = get_template();
-
     	  buffer = get_template();
 
-    	  leds_on (LEDS_RED);
+    	  if( (buffer = get_template()) == NULL) {
+
+    		  debug(LEDS_RED | LEDS_BLUE);
+    		  break;
+    	  }
+
+    	  leds_on (LEDS_BLUE);
     	  clock_delay (500);
-    	  leds_off (LEDS_RED);
+    	  leds_off (LEDS_BLUE);
 
     	  send_msg(buffer, buffer[1]);
 
     	  etimer_reset(&template_timer);
       } else if(etimer_expired(&data_timer)) {
 
-    	  buffer = get_data();
+    	  if( (buffer = get_data()) == NULL) {
+
+    		  debug(LEDS_RED | LEDS_GREEN);
+    		  break;
+    	  }
 
     	  leds_on (LEDS_GREEN);
     	  clock_delay (500);
@@ -63,4 +71,10 @@ PROCESS_THREAD(main_proc, ev, data)
   PROCESS_END();
 }
 
+void debug(unsigned char config) {
 
+	  leds_off(LEDS_ALL);
+	  leds_on(config);
+	  clock_wait(CLOCK_SECOND);
+	  leds_off(config);
+}
